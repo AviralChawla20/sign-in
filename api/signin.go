dea8eba10aeca7a890ff6a3f866b22f6c2e0f31d
Aviral Chawla <aviralchawla02@gmail.com>
2023-11-23 22:25:38 +0530

    Update signin.go

--- api/signin.go
+++ api/signin.go
@@ -4,45 +4,53 @@ import (
 	"encoding/json"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
+	"sync"
 )
 
 // SigninHandler handles user sign-in
 func SigninHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the request body
-	var inputUser User
-	err := json.NewDecoder(r.Body).Decode(&inputUser)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-	// Query the database to find the user by email
-	user, err := FindUserByEmail(inputUser.Email)
-	if err != nil {
-		// If the user is not found, return an error response in JSON format
-		errorResponse := ErrorResponse{Message: "User not found"}
+	go func() {
+		defer wg.Done()
+
+		// Parse the request body
+		var inputUser User
+		err := json.NewDecoder(r.Body).Decode(&inputUser)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Query the database to find the user by email
+		user, err := FindUserByEmail(inputUser.Email)
+		if err != nil {
+			// If the user is not found, return an error response in JSON format
+			errorResponse := ErrorResponse{Message: "User not found"}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(errorResponse)
+			return
+		}
+
+		// Compare passwords
+		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(inputUser.Password))
+		if err != nil {
+			// If the password is invalid, return an error response in JSON format
+			errorResponse := ErrorResponse{Message: "Invalid password"}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(errorResponse)
+			return
+		}
+
+		// Respond with success message
+		response := map[string]interface{}{"message": "Sign in successful"}
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(errorResponse)
-		return
-	}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	}()
 
-	// Compare passwords
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(inputUser.Password))
-	if err != nil {
-		// If the password is invalid, return an error response in JSON format
-		errorResponse := ErrorResponse{Message: "Invalid password"}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(errorResponse)
-		return
-	}
-
-	// Respond with success message
-	response := map[string]interface{}{"message": "Sign in successful"}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
+	wg.Wait()
 }
-
-// Define other route handlers as needed
