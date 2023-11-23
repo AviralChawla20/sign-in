5890d39b0e088521f80b1299f8acf699ced65ee5
Aviral Chawla <aviralchawla02@gmail.com>
2023-11-23 22:25:12 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"awesomeProject1/api"
 	"fmt"
-	_ "github.com/SparkPost/gosparkpost"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"net/http"
@@ -18,24 +17,12 @@ func initializeRouter() {
 		handlers.AllowedHeaders([]string{"Content-Type"}),
 	)
 
-	// Attach your API handlers to the router
 	r.HandleFunc("/signin", api.SigninHandler).Methods("POST")
-	//r.HandleFunc("/users", GetUsers).Methods("GET")
-	//r.HandleFunc("/users/{id}", GetUser).Methods("GET")
-	//r.HandleFunc("/users", CreateUser).Methods("POST")
-	//r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	//r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
 
-	// Function to verify a plaintext password against a stored hash
-
-	// Apply the CORS middleware
 	http.Handle("/", corsMiddleware(r))
 }
 
 func main() {
-	// Configure the database connection
-	//apiKey := "84000f42afe2ecf004f7769ccd491209145bdc42"
-
 	dbConnStr := "sql12647981:XM51KVKzDA@tcp(sql12.freemysqlhosting.net:3306)/sql12647981"
 
 	if err := api.InitializeDB(dbConnStr); err != nil {
@@ -44,30 +31,8 @@ func main() {
 	defer api.CloseDB()
 
 	initializeRouter()
-	// Define CORS options to allow any origin, methods, and headers
-	//corsOptions := handlers.AllowedOrigins([]string{"*"})
-
-	//r := mux.NewRouter()
-	//
-	//// Define CORS options to allow specific origins, methods, and headers
-	//allowedOrigins := handlers.AllowedOrigins([]string{"*"})
-	//allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
-	//allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
-	//
-	//// Apply the CORS middleware to your router with the defined options
-	//cors := handlers.CORS(
-	//	allowedOrigins,
-	//	allowedMethods,
-	//	allowedHeaders,
-	//	handlers.ExposedHeaders([]string{"Content-Type", "Authorization"}),
-	//)(r)
-
-	// Define your API routes
-	// r.HandleFunc("/send-email", sendEmailHandler).Methods("POST")
-	//r.HandleFunc("/signin", api.SigninHandler).Methods("POST")
 
 	port := ":8080"
 	fmt.Printf("Server is running on port %s...\n", port)
 	http.ListenAndServe(port, nil)
-
 }
